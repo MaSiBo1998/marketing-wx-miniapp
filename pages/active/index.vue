815a065df8855ab5b3a6fd155f9856e4b9f28806
wang <2515173971@qq.com>
2025-03-03 23:41:16 +0800

    提交

---
+++ pages/active/index.vue
@@ -0,0 +1,158 @@
+<template>
+	<view class="content">
+		<u-search shape="round" actionText="" v-model="searchVal"></u-search>
+		<view class="search-box">
+			<view class="search-box-item">
+				全部
+			</view>
+			<view class="search-box-item">
+				全部分类
+			</view>
+			<view class="search-box-item">
+				全部分类
+			</view>
+		</view>
+		<view class="active-box" v-for="item,index in list" :key="index">
+			<image class="active-img" src="@/static/active/active-img.png" mode="widthFix"></image>
+			<view class="active-box-title">
+				{{item.name}}
+			</view>
+			<view class="active-box-info">
+				{{item.info}}
+			</view>
+			<view class="active-box-bottom">
+				<view class="active-box-bottom-left">
+					活动时间:{{item.time}} <br>
+					报名截止: {{item.delineTime}}
+				</view>
+				<view class="active-box-bottom-right" @click="toActivePage(item)">
+					立即报名
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		getMyInfoData,
+		getAddressBylat,
+		loginOut
+	} from '@/util/api.js'
+	export default {
+		onShow(option) {},
+		data() {
+			return {
+				searchVal: '',
+				list:[{
+					time:'2025-03-01-04-01',
+					delineTime:'2025-02-28',
+					name:'文艺演出',
+					info: '本活动内容简介内容简介内容简介本活动内容简介内容简介内容简介本活动内容简介内容简介内容简介简介本活动内容简介内容'
+				}],
+			}
+		},
+		computed: {},
+		created() {},
+		methods: {
+			toActivePage(data) {
+				uni.setStorageSync('active_detail',data)
+				uni.navigateTo({
+					url:'/pages/active/detail/index'
+				})
+			}
+		},
+		onLoad() {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		width: 750rpx;
+		padding-bottom: 20rpx;
+
+		/deep/.u-search {
+			width: 686rpx !important;
+			margin: 16rpx auto !important;
+		}
+
+		/deep/.u-search__content {
+			width: 686rpx !important;
+			margin: 0 auto;
+			border-radius: 12rpx !important;
+		}
+
+		/deep/.u-search__action {
+			display: none;
+		}
+
+		.search-box {
+			margin-bottom: 32rpx;
+			width: 750rpx;
+			padding: 0 32rpx 16rpx;
+			border-bottom: 2rpx solid #f5f5f5;
+			display: flex;
+			align-items: center;
+			justify-content: flex-start;
+
+			.search-box-item {
+				margin-right: 16rpx;
+				background-color: #f5f5f5;
+				border-radius: 32rpx;
+				padding: 8rpx 16rpx;
+				font-family: "SimSun", "宋体", serif;
+				font-size: 18rpx;
+			}
+		}
+
+		.active-box {
+			width: 686rpx;
+			margin: 0 auto 16rpx;
+			border-bottom: 2rpx solid #f5f5f5;
+
+			.active-img {
+				width: 686rpx;
+				display: block;
+			}
+
+			.active-box-title {
+				font-size: 36rpx;
+				font-weight: 600;
+				margin: 16rpx 0 6rpx;
+				text-align: left;
+			}
+
+			.active-box-info {
+				font-size: 22rpx;
+				text-align: left;
+				font-weight: 500;
+			}
+
+			.active-box-bottom {
+				margin: 16rpx auto;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+
+				.active-box-bottom-left {
+					font-size: 22rpx;
+					text-align: left;
+					font-weight: 500;
+				}
+
+				.active-box-bottom-right {
+					background-color: rgb(51, 51, 51);
+					padding: 0 32rpx;
+					height: 64rpx;
+					line-height: 64rpx;
+					font-size: 22rpx;
+					color: #FFF;
+					border-radius: 56rpx;
+					font-weight: 600;
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/actorList/actorDetail/index.vue
@@ -0,0 +1,150 @@
+<template>
+	<view class="content">
+		<view class="actor-box">
+			<view class="actor-box-top">
+				<view class="actor-box-top-left">
+					<view class="actor-box-item">
+						<view class="actor-box-item-left">
+							<text class="title">姓名:</text>
+							<text class="info">张三</text>
+						</view>
+						<view class="actor-box-item-right">
+							<text class="title">性别:</text>
+							<text class="info">男</text>
+						</view>
+						
+					</view>
+					<view class="actor-box-item">
+						<view class="actor-box-item-left">
+							<text class="title">年龄:</text>
+							<text class="info">18</text>
+						</view>
+						<view class="actor-box-item-right">
+							<text class="title">身高:</text>
+							<text class="info">180</text>
+						</view>
+						
+					</view>
+					<view class="actor-box-item">
+						<view class="actor-box-item-left">
+							<text class="title">体重:</text>
+							<text class="info">130</text>
+						</view>
+						<view class="actor-box-item-right">
+							<text class="title">特长:</text>
+							<text class="info">唱歌</text>
+						</view>
+						
+					</view>
+				
+				</view>
+				<image class="actor-box-top-right" src="@/static/home/home-top-banner.png" mode="widthFix"></image>
+				
+			</view>
+			<view class="actor-detail-title">
+				演员详细资料
+			</view>
+			<view class="actor-detail-info">
+				详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料详细资料
+				详细资料
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	import {
+		getMyInfoData,
+		getAddressBylat,
+		loginOut
+	} from '@/util/api.js'
+	export default {
+		onShow(option) {},
+		data() {
+			return {
+				searchVal: '',
+			}
+		},
+		computed: {},
+		methods: {
+
+		},
+		onUnload() {
+			uni.removeStorageSync('actor_detail')
+
+
+		},
+		onLoad() {
+			console.log(uni.getStorageSync('actor_detail'))
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		width: 750rpx;
+		height: 582rpx;
+		background: linear-gradient(7.73deg, rgba(255, 255, 255, 0) 7.45%, rgba(255, 106, 12, 1) 61.97%, rgba(255, 106, 12, 1) 95.12%) no-repeat;
+		background-size: 750rpx 582rpx;
+		background-color: #EEE;
+		min-height: 100vh;
+		padding-top: 20rpx;
+		box-sizing: border-box;
+
+		.actor-box {
+			width: 686rpx;
+			margin: 0 auto;
+			box-sizing: border-box;
+			padding: 48rpx 32rpx;
+			border-radius: 24rpx;
+			background: rgba(255, 255, 255, 1);
+			min-height: 80vh;
+			.actor-box-item {
+				display: flex;
+				align-items: center;
+				justify-content: flex-start;
+				width: 436rpx;
+				margin-bottom: 16rpx;
+				.actor-box-item-left,.actor-box-item-right {
+					width: 218rpx;
+				}
+				.title {
+					font-weight: 600;
+					font-size: 26rpx;
+					text-align: left;
+				}
+				.info {
+					color: rgb(153, 153, 153);
+					font-size: 32rpx;
+				}
+			}
+			.actor-box-top {
+				display: flex;
+				align-items: flex-start;
+				justify-content: space-between;
+				.actor-box-top-left {
+
+				}
+				.actor-box-top-right {
+					width: 218rpx;
+					display: block;
+				}
+			}
+			.actor-detail-title {
+				padding: 15rpx 0;
+				border-top: 1rpx solid #EEE;
+				font-size: 26rpx;
+				font-weight: 600;
+				margin: 16rpx 0 6rpx;
+				text-align: left;
+			}
+			.actor-detail-info {
+				font-size: 22rpx;
+				text-align: left;
+				font-weight: 500;
+				color: rgb(153, 153, 153);
+			}
+		}
+	}
+</style>
